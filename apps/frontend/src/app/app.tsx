c31bfc906015187f06a63ae855d3fa28aafca9fd
aliskhanoff <sociprogrammer@gmail.com>
2024-10-06 16:42:17 +0200

    fix: clean app from stubs

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,13 +1,8 @@
-import { IconicInput } from '@olympicoin/shared'
-import { AiFillAccountBook, AiFillWallet } from 'react-icons/ai'
 
 export const App = () => {
   return (
     <div className='w-full h-80 flex flex-col items-start justify-start p-10 gap-5'>
-        <IconicInput rounded sizeVariant='sm' iconLeft={ <AiFillWallet /> } placeholder='Hello world' styleVariant='primary' />
-        <IconicInput rounded sizeVariant='md' iconLeft={ <AiFillAccountBook /> } placeholder='Hello world' styleVariant='danger' />
-        <IconicInput rounded sizeVariant='lg' iconLeft={ <AiFillAccountBook /> } placeholder='Hello world' styleVariant='warning' />
-        <IconicInput rounded sizeVariant='xl' iconLeft={ <AiFillAccountBook /> } placeholder='Hello world' styleVariant='info' />
+       
     </div>
   );
 }
